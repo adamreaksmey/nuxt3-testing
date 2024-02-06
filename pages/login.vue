f6a024cb0c98d8636f4388cd8cb93160ae57031f
adamreaksmey <adaminiature@gmail.com>
2024-02-06 22:13:23 +0700

    init some files

---
+++ pages/login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="title">
+      <h2>Login</h2>
+    </div>
+    <div class="container form">
+      <label for="uname"><b>Username</b></label>
+      <input
+        v-model="user.username"
+        type="text"
+        class="input"
+        placeholder="Enter Username"
+        name="uname"
+        required
+      />
+
+      <label for="psw"><b>Password</b></label>
+      <input
+        v-model="user.password"
+        type="password"
+        class="input"
+        placeholder="Enter Password"
+        name="psw"
+        required
+      />
+
+      <button @click.prevent="login" class="button">Login</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const user = ref({
+  username: "",
+  password: "",
+});
+
+const login = async () => {
+  console.log(user.value);
+};
+</script>
+
+<style lang="scss" scoped></style>

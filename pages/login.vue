89f830966bbe67cc1f4699622215783aa1776066
adamreaksmey <adaminiature@gmail.com>
2024-02-06 22:20:13 +0700

    added login page

--- pages/login.vue
+++ pages/login.vue
@@ -30,13 +30,25 @@
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from "pinia"; // import storeToRefs helper hook from pinia
+import { useAuthStore } from "~/store/auth"; // import the auth store we just created
+
+const { authenticateUser } = useAuthStore(); // use authenticateUser action from  auth store
+
+const { authenticated } = storeToRefs(useAuthStore()); // make authenticated state reactive with storeToRefs
+
 const user = ref({
-  username: "",
-  password: "",
+  username: "kminchelle",
+  password: "0lelplR",
 });
+const router = useRouter();
 
 const login = async () => {
-  console.log(user.value);
+  await authenticateUser(user.value); // call authenticateUser and pass the user object
+  // redirect to homepage if user is authenticated
+  if (authenticated) {
+    router.push("/");
+  }
 };
 </script>
 
